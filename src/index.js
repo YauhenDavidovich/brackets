ee5cd3525c982a43def1803f518d6e15d0e98dc6
Евгений Давидович <davidovichyauhen@gmail.com>
2019-10-13 12:44:52 +0300

    feat: solution for task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    for (let i = 0; i <= str.length * 2; i++) {
+        for (let j = 0; j < bracketsConfig.length; j++) {
+            let brackets_sequence = bracketsConfig[j].join(""); //convert bracketsConfig into a string          
+            while (str.includes(brackets_sequence)) {
+                str = str.replace(brackets_sequence, ""); //remove the set from the string by the cycle     
+            }
+        }
+    }
+    if (str.length === 0) {
+        return true; //If the string is empty, the brackets sequence  is correct 
+    } else {
+        return false;
+    }
 }
